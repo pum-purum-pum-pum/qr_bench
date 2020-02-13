8941a8103b64561faa13405d8f1627ffee89413a
Vlad <vladzhukovtirko@gmail.com>
2020-02-13 15:24:54 +0300

    images reader

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use bardecoder;
 use crossbeam_utils::thread;
 use image::imageops::FilterType;
 use std::time::Instant;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use bardecoder::prepare::BlockedMean;
 const MSG_NUM: usize = 120;
 
 fn main() {
@@ -9,12 +12,20 @@ fn main() {
     let now = Instant::now();
     thread::scope(|s| {
         let mut threads = vec![];
-        for _ in 0..MSG_NUM {
-            let img = image::open("test.jpg").unwrap();
+        // for _ in 0..MSG_NUM {
+        for entry in fs::read_dir("images").unwrap() {
+        	// let filename = entry.unwrap().file_name();
+        	// dbg!(&filename);
+
+            // let mut img = image::open("IMG_2186.jpg").unwrap();
+            // img = img.grayscale();
+            let img = image::open(entry.unwrap().path()).unwrap();
             // images.push(img);
             let handle = s.spawn(move |_| {
-                let decoder = bardecoder::default_decoder();
+                let mut decoder = bardecoder::default_decoder();
+                // decoder.prepare(Box::new(BlockedMean::new(7, 9)));
                 let img = img.resize(800, 600, FilterType::Nearest);
+	            // img.save(Path::new("resized.jpg"));
                 let results = decoder.decode(img);
                 println!("{:?}", results);
             });
