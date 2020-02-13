390e2e946e48cf4815f90108ffdaadfdbc9918bb
Vlad <vladzhukovtirko@gmail.com>
2020-02-13 13:57:46 +0300

    fmt pass

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,21 @@ fn main() {
     // Use default decoder
     let now = Instant::now();
     thread::scope(|s| {
-    	let mut threads = vec![];
-		for _ in 0..MSG_NUM {
-		    let img = image::open("test.jpg").unwrap();
-		    // images.push(img);
+        let mut threads = vec![];
+        for _ in 0..MSG_NUM {
+            let img = image::open("test.jpg").unwrap();
+            // images.push(img);
             let handle = s.spawn(move |_| {
-			    let decoder = bardecoder::default_decoder();
+                let decoder = bardecoder::default_decoder();
                 let img = img.resize(800, 600, FilterType::Nearest);
                 let results = decoder.decode(img);
                 println!("{:?}", results);
             });
             threads.push(handle);
-		    println!("{:?}", Instant::now() - now);
-		}
+            println!("{:?}", Instant::now() - now);
+        }
         for thread in threads.drain(..) {
-        	thread.join().unwrap();
+            thread.join().unwrap();
         }
     })
     .unwrap();
