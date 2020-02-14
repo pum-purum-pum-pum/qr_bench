26e78fa852ee72012f0b062fe8d8164674309304
Vlad <vladzhukovtirko@gmail.com>
2020-02-14 11:20:32 +0300

    with quirc

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
-use bardecoder;
 use crossbeam_utils::thread;
-use image::imageops::FilterType;
+use image;
+// another library to try for this task
+use quirc;
+
+use std::fs::{self, File};
+use std::io::Read;
 use std::time::Instant;
-use std::fs::{self, DirEntry};
-use std::path::Path;
-use bardecoder::prepare::BlockedMean;
-const MSG_NUM: usize = 120;
+use std::str::from_utf8;
+use quirc::QrCoder;
+use image::imageops::FilterType;
 
 fn main() {
-    // Use default decoder
-    let now = Instant::now();
+    let start = Instant::now();
     thread::scope(|s| {
-        let mut threads = vec![];
-        // for _ in 0..MSG_NUM {
         for entry in fs::read_dir("images").unwrap() {
-        	// let filename = entry.unwrap().file_name();
-        	// dbg!(&filename);
-
-            // let mut img = image::open("IMG_2186.jpg").unwrap();
-            // img = img.grayscale();
-            let img = image::open(entry.unwrap().path()).unwrap();
-            // images.push(img);
-            let handle = s.spawn(move |_| {
-                let mut decoder = bardecoder::default_decoder();
-                // decoder.prepare(Box::new(BlockedMean::new(7, 9)));
-                let img = img.resize(800, 600, FilterType::Nearest);
-	            // img.save(Path::new("resized.jpg"));
-                let results = decoder.decode(&img);
-                println!("{:?}", results);
+            let mut file = File::open(entry.unwrap().path()).unwrap();
+            let mut vec = Vec::new();
+            file.read_to_end(&mut vec).unwrap();
+            let image = image::load_from_memory(&vec).unwrap().resize(800, 600, FilterType::Nearest).to_luma();
+            let width  = image.width();
+            let height = image.height();        
+            let _handle = s.spawn(move |_| {
+                let mut quirc = QrCoder::new().unwrap();
+                let codes  = quirc.codes(&image, width, height).unwrap();
+                for code in codes {
+                    match code {
+                        Ok(code) => {
+                            println!("{:?}", code);
+                            println!("{:?}", from_utf8(&code.payload))
+                        },
+                        Err(err) => println!("Error: {:?}", err),
+                    }
+                }
             });
-            threads.push(handle);
-            println!("{:?}", Instant::now() - now);
-        }
-        for thread in threads.drain(..) {
-            thread.join().unwrap();
+            dbg!(Instant::now() - start);
         }
     })
-    .unwrap();
+    .unwrap()
 }
